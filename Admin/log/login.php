1d9d5008e5ea2f86b433b1897c5a4a6719097d02
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2023-08-23 10:54:14 +0530

    add admin pages

---
+++ Admin/log/login.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    include "connect/connect.php";
+
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+
+
+
+    $sql1 = mysqli_query($conn,"SELECT * FROM admin WHERE username = '$username' AND password = '$password'");
+    $result = mysqli_num_rows($sql1);
+    
+    if($result > 0 )
+    { 
+        header('Location: ../signup.html');
+        exit;    
+    }
+    else
+    {
+        echo 'The username or password are incorrect!';
+    }
+?> 
